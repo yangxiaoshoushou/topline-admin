18869d8da2d8914b1c4987e2542eab0533103f06
yangping <2506186636@.qq.com>
2019-06-24 21:53:56 +0800

    Layout组件-NavMenu导航菜单

--- src/views/layout/components/AppAside.vue
+++ src/views/layout/components/AppAside.vue
@@ -1,7 +1,66 @@
 <template>
-  <div>
-    <p>AppAside Component</p>
-  </div>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      router
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="2">
+        <i class="el-icon-s-home"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>内容管理</span>
+        </template>
+        <el-menu-item index="1-2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="1-3">
+          <i class="el-icon-menu"></i>
+          <span slot="title">内容列表</span>
+        </el-menu-item>
+        <el-menu-item index="1-4">
+          <i class="el-icon-menu"></i>
+          <span slot="title">评论列表</span>
+        </el-menu-item>
+        <el-menu-item index="1-5">
+          <i class="el-icon-menu"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>粉丝管理</span>
+        </template>
+        <el-menu-item index="2-2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">图文数据</span>
+        </el-menu-item>
+        <el-menu-item index="2-3">
+          <i class="el-icon-menu"></i>
+          <span slot="title">粉丝概况</span>
+        </el-menu-item>
+        <el-menu-item index="2-4">
+          <i class="el-icon-menu"></i>
+          <span slot="title">粉丝画像</span>
+        </el-menu-item>
+        <el-menu-item index="2-5">
+          <i class="el-icon-menu"></i>
+          <span slot="title">粉丝列表</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">账户设置</span>
+      </el-menu-item>
+    </el-menu>
 </template>
 
 <script>
@@ -9,10 +68,20 @@ export default {
   name: 'AppAside',
   data () {
     return {}
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.el-menu {
+  height: 100%;
+}
 </style>
