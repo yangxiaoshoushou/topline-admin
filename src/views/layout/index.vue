2e5e6fcdb2fc4c8ee89ee9806b8170350f33f758
yangping <2506186636@.qq.com>
2019-06-24 21:20:16 +0800

    Layout组件-Container布局容器

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    <AppHeader/>
-    <AppAside/>
-  </div>
+  <el-container>
+    <el-aside width="200px">
+      <AppAside/>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <AppHeader/>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -22,5 +29,16 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.el-container {
+  height: 100%;
+}
+.el-aside {
+  background-color: #d0dce7;
+}
+.el-header {
+  background-color: #aec0d1;
+}
+.el-main {
+  background-color: #e8eff4;
+}
 </style>
