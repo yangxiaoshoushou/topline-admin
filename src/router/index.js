886b84b17e535c06f6013a92c998cdbdd4c8d523
yangping <2506186636@.qq.com>
2019-06-25 21:34:39 +0800

    使用路由拦截器处理页面访问权限.itcast

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     // {
     //   name: 'home',
@@ -11,7 +11,7 @@ export default new Router({
     //   component: () => import('@/views/home')
     // },
     {
-      name: 'layout',
+      // name: 'layout',
       path: '/',
       component: () => import('@/views/layout'),
       children: [
@@ -34,3 +34,28 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const userInfo = window.localStorage.getItem('user_info')
+
+  // 如果是非 /login 页面，判断其登录状态
+  if (to.path !== '/login') {
+    // 如果没有登录，让其跳转到登录页
+    if (!userInfo) {
+      next({ name: 'login' })
+    } else {
+      // 如果登录了，则允许通过
+      next()
+    }
+  } else {
+    // 如果登录了，就不允许访问登录页了
+    if (userInfo) {
+      next(false)
+    } else {
+      // 没有登录，才允许访问登录页
+      next()
+    }
+  }
+})
+
+export default router
