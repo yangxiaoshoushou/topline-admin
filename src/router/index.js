c13c3f5b77df589c0d3896a7c2ebcd14dc729fca
yangping <2506186636@.qq.com>
2019-06-26 09:47:33 +0800

    文章列表-创建组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -25,40 +25,45 @@ const router = new Router({
           name: 'publish',
           path: '/publish',
           component: () => import('@/views/publish')
+        },
+        {
+          name: 'article-list',
+          path: '/article',
+          component: () => import('@/views/article')
         }
       ]
     },
     {
-      name: 'Login',
+      name: '',
       path: '/Login',
       component: () => import('@/views/Login')
     }
   ]
 })
 
-router.beforeEach((to, from, next) => {
-  nprogress.start()
-  const userInfo = window.localStorage.getItem('user_info')
+// router.beforeEach((to, from, next) => {
+//   nprogress.start()
+//   const userInfo = window.localStorage.getItem('user_info')
 
-  // 如果是非 /login 页面，判断其登录状态
-  if (to.path !== '/login') {
-    // 如果没有登录，让其跳转到登录页
-    if (!userInfo) {
-      next({ name: 'login' })
-    } else {
-      // 如果登录了，则允许通过
-      next()
-    }
-  } else {
-    // 如果登录了，就不允许访问登录页了
-    if (userInfo) {
-      next(false)
-    } else {
-      // 没有登录，才允许访问登录页
-      next()
-    }
-  }
-})
+//   // 如果是非 /login 页面，判断其登录状态
+//   if (to.path !== '/login') {
+//     // 如果没有登录，让其跳转到登录页
+//     if (!userInfo) {
+//       next({ name: 'login' })
+//     } else {
+//       // 如果登录了，则允许通过
+//       next()
+//     }
+//   } else {
+//     // 如果登录了，就不允许访问登录页了
+//     if (userInfo) {
+//       next(false)
+//     } else {
+//       // 没有登录，才允许访问登录页
+//       next()
+//     }
+//   }
+// })
 router.afterEach((to, from) => {
   nprogress.done()
 })
---
+++ src/views/article/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Article Component</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleList',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
