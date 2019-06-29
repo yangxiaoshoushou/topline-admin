ffe2cba36fdfd68fd7e50cb2730951887043a0cd
yangping <2506186636@.qq.com>
2019-06-29 22:29:49 +0800

    富文本编辑器的使用

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,11 +9,14 @@
 
   </div>
   <el-form>
-    <el-form-item label="标题">
-      <el-input type="text" v-model="articleForm.title"></el-input>
+    <el-form-item>
+      <el-input type="text" v-model="articleForm.title" placeholder="标题"></el-input>
     </el-form-item>
-    <el-form-item label="内容">
-      <el-input type="textarea" v-model="articleForm.content"></el-input>
+    <el-form-item>
+      <quill-editor v-model="articleForm.content"
+        ref="myQuillEditor"
+        :options="editorOption">
+      </quill-editor>
     </el-form-item>
     <el-form-item label="封面">
     </el-form-item>
@@ -28,10 +31,16 @@
 
 <script>
 import ArticleChannel from '@/components/article-channel'
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
 export default {
   name: 'AppPublish',
   components: {
-    ArticleChannel
+    ArticleChannel,
+    quillEditor
   },
   data () {
     return {
@@ -43,8 +52,17 @@ export default {
           images: []
         },
         channel_id: 3
-      }
+      },
+      editorOption: {}
     }
+  },
+  computed: {
+    editor () {
+      return this.$refs.myQuillEditor.quill
+    }
+  },
+  mounted () {
+    console.log('this is current quill instance object', this.editor)
   },
   methods: {
     handlePublish (draft = false) {
